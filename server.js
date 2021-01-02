eeba485f3e151a8aea0c245b388563d57e3f6dc1
Abhinav Gangula <gangulaabhinav@gmail.com>
2021-01-02 19:42:18 +0530

    Adding pug package to the app. Using it to create a view for home page.

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 'use strict'
 const express = require('express')
+const path = require('path')
 
 const app = express()
 const port = process.env.PORT || 1337
 
+// Setting view engine
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
+
 // Support get request for home page
 app.get('/', (req, res) => {
-    res.send('Hello Express')
+    res.render('index', {title:'Pug'})
 })
 
 // Start listening to app requests
